4d2aa0958a5755c6f477e1fa0758948fcf6e1888
Suzanne Blair <suzanneblair@Suzannes-MacBook-Pro.local>
2017-05-31 14:18:13 -0400

    revised search bar, added video-list and video-list-item

---
+++ src/components/video_list.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import VideoList Item from './vide_list_item'
+
+const VideoList = (props) => {
+  return (
+    <ul className="col-md-4 list-group">
+    </ul>
+  )
+}
+
+export default VideoList
