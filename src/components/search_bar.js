0d99048570d6927add7279cc247397c96ffc523b
Suzanne Blair <suzanneblair@Suzannes-MacBook-Pro.local>
2017-08-22 15:01:52 -0400

    added search capability with searchbar component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -13,10 +13,18 @@ class SearchBar extends Component {
       <div className="search-bar">
         <input
           value = {this.state.term}
-          onChange={e=>this.setState({ term: e.target.value })}/>
+          onChange={e=>this.onInputChange(e.target.value)}/>
       </div>
       )
   }
+
+onInputChange(term) {
+  this.setState({term})
+  this.props.onSearchTermChange(term)
+
+
+}
+
 }
 
 
